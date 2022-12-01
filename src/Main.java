600a452ec77953fb1648a6b650c6464941aaf608
anna <anna@sayf.su>
2022-12-01 12:10:41 +0300

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int a = 55_555;
+        System.out.println("Значение переменной a с типом int равно " + a);
+        byte b = 5;
+        System.out.println("Значение переменной b с типом byte равно " + b);
+        short c = 5_555;
+        System.out.println("Значение переменной c с типом short равно " + c);
+        long d = 5_555_555L;
+        System.out.println("Значение переменной d с типом long равно " + d);
+        float e = 5.5555f;
+        System.out.println("Значение переменной e с типом float равно " + e);
+        double f = 265.56556547;
+        System.out.println("Значение переменной f с типом double равно " + f);
+
+
+
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+
     }
 }
