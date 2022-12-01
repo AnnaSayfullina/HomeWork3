2a75fb35058e39fadf8f5089c00f95b066155dc9
anna <anna@sayf.su>
2022-12-01 21:00:01 +0300

    задача 4

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
+
     }
 
     public static void task1() {
@@ -45,6 +47,22 @@ public class Main {
         byte totalPupils = (byte) (pupilLudPav + pupilAnSer + pupilEkAn);
         short paperOnePupil = (short) (totalPaper / totalPupils);
         System.out.println("На каждого ученика рассчитано " + paperOnePupil + " листов бумаги");
+    }
+    public static void task4() {
+        System.out.println("Задача 4");
+        byte productivityOneMin = 16 / 2;
+        byte hour = 60;
+        short day = (short) (hour * 24);
+        int month = day * 30;
+        short productivity20Min = (short) (productivityOneMin * 20);
+        System.out.println("За 20 минут машина произвела " + productivity20Min + " штук бутылок");
+        short productivityDay = (short) (productivityOneMin * day);
+        System.out.println("За сутки машина произвела " + productivityDay + " штук бутылок");
+        int productivity3Day = productivityDay * 3;
+        System.out.println("За 3 дня машина произвела " + productivity3Day + " штук бутылок");
+        int productivityOneMonth = productivityOneMin * month;
+        System.out.println("За месяц машина произвела " + productivityOneMonth + " штук бутылок");
+
 
 
     }
